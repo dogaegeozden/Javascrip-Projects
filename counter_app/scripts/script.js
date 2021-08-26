b7c8f28fe58b98999c4ab5a5364f1955c0c868a5
Doga <61098483+dogaegeozden@users.noreply.github.com>
2021-08-26 10:13:36 -0700

    Add files via upload

---
+++ counter_app/scripts/script.js
@@ -0,0 +1,16 @@
+let numberArea = document.querySelector("h1");
+let increaseButton = document.querySelector(".increase");
+let decreaseButton = document.querySelector(".decrease");
+
+let currentNumber = document.querySelector("h1").textContent;
+let numberGenerator = parseInt(currentNumber);
+
+increaseButton.onclick = function() {
+    numberGenerator += 1;
+    numberArea.innerHTML = numberGenerator;
+}
+
+decreaseButton.onclick = function () {
+    numberGenerator -= 1;
+    numberArea.innerHTML = numberGenerator;
+}
